142071f333dfbea509295440b1be2b185bf45a93
Kamil <kamgre7@gmail.com>
2024-01-26 19:41:55 +0100

    Added api types

---
+++ src/apiTypes.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Request } from 'express';
+import { ParsedQs } from 'qs';
+
+export type ExpressRequest<
+  T extends {
+    query?: Record<string, unknown>;
+    body?: Record<string, unknown>;
+    params?: Record<string, unknown>;
+    locals: Record<string, unknown>;
+  },
+> = Request<T['params'], {}, T['body'], T['query'] & ParsedQs, T['locals']>;
+
+export type ParsedRequest<
+  T extends {
+    query?: Record<string, unknown>;
+    body?: Record<string, unknown>;
+    params?: Record<string, unknown>;
+  },
+> = ExpressRequest<T & { locals: {} }>;
