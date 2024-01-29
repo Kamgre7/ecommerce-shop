d0b24622cce4a92732d47872f43760904d7e4ad6
Kamil <kamgre7@gmail.com>
2024-01-29 22:12:20 +0100

    Created tables products, categories schema

---
+++ src/database/migrations/1706476926_createProductsInventoryTable.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('products_inventory')
+    .addColumn('id', 'uuid', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('quantity', 'integer', (col) => col.notNull())
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`))
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`))
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('products_inventory').execute();
+}
---
+++ src/database/migrations/1706560837_createProductTable.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('products')
+    .addColumn('id', 'uuid', (col) => col.primaryKey().defaultTo(sql`gen_random_uuid()`))
+    .addColumn('name', 'varchar(25)', (col) => col.unique().notNull())
+    .addColumn('description', 'varchar(1000)', (col) => col.notNull())
+    .addColumn('price', 'integer', (col) => col.notNull())
+    .addColumn('sku', 'varchar(5)', (col) => col.notNull())
+    .addColumn('category_id', 'uuid', (col) => col.references('categories.id'))
+    .addColumn('inventory_id', 'uuid', (col) => col.unique().references('products_inventory.id'))
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`))
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`))
+    .addColumn('deleted_at', 'timestamp', (col) => col.defaultTo(null))
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('products').execute();
+}
