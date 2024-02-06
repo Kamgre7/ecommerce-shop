165d22b6aa8d13f4699e9661da842513d51f3e05
Kamil <kamgre7@gmail.com>
2024-02-06 22:15:27 +0100

    Added products route to main router

--- src/routes/mainRouter.ts
+++ src/routes/mainRouter.ts
@@ -1,3 +1,6 @@
 import { Router } from 'express';
+import { productsRouter } from '../domains/products/routes/productRoutes';
 
 export const mainRouter = Router();
+
+mainRouter.use('/products', productsRouter);
