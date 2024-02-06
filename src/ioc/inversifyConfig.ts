1dfc47af69387ccec64d196a4b33f11d8a58718b
Kamil <kamgre7@gmail.com>
2024-02-06 22:13:33 +0100

    Added inversify types

--- src/ioc/inversifyConfig.ts
+++ src/ioc/inversifyConfig.ts
@@ -1,3 +1,16 @@
 import { Container } from 'inversify';
+import { TYPES } from './types/types';
+import { IProductsController, ProductsController } from '../domains/products/controllers/productController';
+import { IProductsService, ProductsService } from '../domains/products/services/productService';
+import { IProductsRepository, ProductsRepository } from '../domains/products/repository/productRepository';
+import { ErrorMapper, IErrorMapper } from '../errors/errorMapper';
 
 export const container = new Container();
+
+// Product
+container.bind<IProductsController>(TYPES.ProductsControllerToken).to(ProductsController);
+container.bind<IProductsService>(TYPES.ProductsServiceToken).to(ProductsService);
+container.bind<IProductsRepository>(TYPES.ProductsRepositoryToken).to(ProductsRepository);
+
+// ErrorMapper
+container.bind<IErrorMapper>(TYPES.ErrorMapperToken).to(ErrorMapper);
--- src/ioc/types/types.ts
+++ src/ioc/types/types.ts
@@ -1,1 +1,7 @@
-export const TYPES = {};
+export const TYPES = {
+  ProductsControllerToken: Symbol.for('IProductsController'),
+  ProductsServiceToken: Symbol.for('IProductsService'),
+  ProductsRepositoryToken: Symbol.for('IProductsRepository'),
+
+  ErrorMapperToken: Symbol.for('IErrorMapper'),
+};
