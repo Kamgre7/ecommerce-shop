ce483af332c97f44bb6dcf7e5310970acd503df2
Kamil <kamgre7@gmail.com>
2024-02-23 19:13:27 +0100

    Added product inventory model

--- src/domains/products/models/ProductInventory.ts
+++ src/domains/products/models/ProductInventory.ts
@@ -1,3 +1,5 @@
+import { Exclude } from 'class-transformer';
+
 export interface IProductInventory {
   id: string;
   quantity: number;
@@ -8,13 +10,10 @@ export interface IProductInventory {
 export class ProductInventory implements IProductInventory {
   id: string;
   quantity: number;
-  createdAt: Date;
-  updatedAt: Date;
 
-  constructor(productInventoryInfo: IProductInventory) {
-    this.id = productInventoryInfo.id;
-    this.quantity = productInventoryInfo.quantity;
-    this.createdAt = productInventoryInfo.createdAt;
-    this.updatedAt = productInventoryInfo.updatedAt;
-  }
+  @Exclude()
+  createdAt: Date;
+
+  @Exclude()
+  updatedAt: Date;
 }
