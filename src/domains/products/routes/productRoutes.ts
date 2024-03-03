276c6b97d268b0e3bdb5f6ba8294c43f5539299b
Kamil <kamgre7@gmail.com>
2024-03-03 22:54:19 +0100

    Added products routes

--- src/domains/products/routes/productRoutes.ts
+++ src/domains/products/routes/productRoutes.ts
@@ -4,9 +4,26 @@ import { CreateProductSchema } from '../schemas/createProductValidationSchema';
 import { container } from '../../../ioc/inversifyConfig';
 import { IProductsController } from '../controllers/productController';
 import { TYPES } from '../../../ioc/types/types';
+import { FindByIdProductSchema } from '../schemas/findProductValidationSchema';
+import { UpdateProductSchema } from '../schemas/updateProductValidationSchema';
+import { UpdateProductInventorySchema } from '../schemas/updateProductInventoryValidationSchema';
+import { FindProductByCriteriaSchema } from '../schemas/findProductByCriteriaValidationSchema';
+import { DeleteProductSchema } from '../schemas/deleteProductValidationSchema';
 
 export const productsRouter = Router();
 
 const productsController = container.get<IProductsController>(TYPES.ProductsControllerToken);
 
 productsRouter.route('/').post(requestValidator(CreateProductSchema), productsController.create);
+
+productsRouter.route('/search').get(requestValidator(FindProductByCriteriaSchema), productsController.findByCriteria);
+
+productsRouter
+  .route('/inventory/:inventoryId')
+  .patch(requestValidator(UpdateProductInventorySchema), productsController.updateInventory);
+
+productsRouter
+  .route('/:id')
+  .get(requestValidator(FindByIdProductSchema), productsController.findById)
+  .patch(requestValidator(UpdateProductSchema), productsController.update)
+  .delete(requestValidator(DeleteProductSchema));
