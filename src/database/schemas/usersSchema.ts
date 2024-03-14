474be9f547df73ffb327dcea0f50941377f7d7c6
Kamil <kamgre7@gmail.com>
2024-03-14 22:51:48 +0100

    Added users table interface

---
+++ src/database/schemas/usersSchema.ts
@@ -0,0 +1,16 @@
+import { Generated } from 'kysely';
+import { GenderType } from '../../domains/user/types/gender';
+import { RoleType } from '../../domains/user/types/userRole';
+
+export interface UsersTable {
+  id: Generated<string>;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  gender: GenderType;
+  role: Generated<RoleType>;
+  createdAt: Generated<Date>;
+  updatedAt: Generated<Date>;
+  deletedAt: Generated<Date> | null;
+}
