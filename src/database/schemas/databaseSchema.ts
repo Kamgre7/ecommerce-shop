e6c6a21e759ba5787657d3c462b2c386889edea6
Kamil <kamgre7@gmail.com>
2024-03-17 22:09:11 +0100

    Added users table to database interface

--- src/database/schemas/databaseSchema.ts
+++ src/database/schemas/databaseSchema.ts
@@ -1,9 +1,11 @@
 import { CategoriesTable } from './categoriesSchema';
 import { ProductsInventoryTable } from './productsInventorySchema';
 import { ProductsTable } from './productsSchema';
+import { UsersTable } from './usersSchema';
 
 export interface Database {
   products: ProductsTable;
   products_inventory: ProductsInventoryTable;
   categories: CategoriesTable;
+  users: UsersTable;
 }
