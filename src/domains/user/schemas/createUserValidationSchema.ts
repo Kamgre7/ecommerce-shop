cc484cb8984c793fc2cb0ac431728d93975e74fc
Kamil <kamgre7@gmail.com>
2024-03-17 22:10:11 +0100

    added create user req validation schema

---
+++ src/domains/user/schemas/createUserValidationSchema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+import { GENDER } from '../types/gender';
+
+// Minimum eight characters, at least one uppercase, one lowercase character, one number and one special character
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const CreateUserBodySchema = z.object({
+  email: z.string().email(),
+  firstName: z.string().min(3),
+  lastName: z.string().min(3),
+  password: z.string().regex(passwordRegex),
+  gender: z.nativeEnum(GENDER),
+});
+
+export const CreateUserSchema = z.object({
+  body: CreateUserBodySchema,
+});
+
+export type CreateUserReq = z.infer<typeof CreateUserSchema>;
+export type NewUser = z.infer<typeof CreateUserBodySchema>;
