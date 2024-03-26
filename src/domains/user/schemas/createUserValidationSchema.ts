cd5195a2bb98f261b978c7d6354b81d223b8330a
Kamil <kamgre7@gmail.com>
2024-03-26 19:17:17 +0100

    Updated schema

--- src/domains/user/schemas/createUserValidationSchema.ts
+++ src/domains/user/schemas/createUserValidationSchema.ts
@@ -1,18 +1,18 @@
-import { z } from 'zod';
+import { nativeEnum, object, string, z } from 'zod';
 import { GENDER } from '../types/gender';
 
 // Minimum eight characters, at least one uppercase, one lowercase character, one number and one special character
 export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-export const CreateUserBodySchema = z.object({
-  email: z.string().email(),
-  firstName: z.string().min(3),
-  lastName: z.string().min(3),
-  password: z.string().regex(passwordRegex),
-  gender: z.nativeEnum(GENDER),
+export const CreateUserBodySchema = object({
+  email: string().email(),
+  firstName: string().min(3),
+  lastName: string().min(3),
+  password: string().regex(passwordRegex),
+  gender: nativeEnum(GENDER),
 });
 
-export const CreateUserSchema = z.object({
+export const CreateUserSchema = object({
   body: CreateUserBodySchema,
 });
 
